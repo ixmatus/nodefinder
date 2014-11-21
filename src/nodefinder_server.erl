4ab6e9689fdef0e2d1516f729e680cfd0384801f
Parnell Springmeyer <parnell@ixmat.us>
2014-11-20 21:55:35 -0600

    Minor cleanup

--- src/nodefinder_server.erl
+++ src/nodefinder_server.erl
@@ -4,15 +4,15 @@
 %% @hidden
 %% @end
 
--module (nodefinder_server).
--behaviour (gen_server).
--export ([ start_link/2, start_link/3, discover/0 ]).
--export ([ init/1,
-           handle_call/3,
-           handle_cast/2,
-           handle_info/2,
-           terminate/2,
-           code_change/3]).
+-module(nodefinder_server).
+-behaviour(gen_server).
+-export([ start_link/2, start_link/3, discover/0 ]).
+-export([ init/1,
+          handle_call/3,
+          handle_cast/2,
+          handle_info/2,
+          terminate/2,
+          code_change/3]).
 
 -oldrecord (state).
 
