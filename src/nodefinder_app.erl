59f18f7ffdd32dcb340777ef7549e72eae70115d
Parnell Springmeyer <parnell@ixmat.us>
2014-11-20 21:42:40 -0600

    Modernizing and adding rebar so I can add lager logs

---
+++ src/nodefinder_app.erl
@@ -0,0 +1,47 @@
+%% @doc Nodefinder service.
+%% @end
+
+-module (nodefinder_app).
+-export ([ discover/0 ]).
+-behaviour (application).
+-export ([ start/0, start/2, stop/0, stop/1 ]).
+
+%-=====================================================================-
+%-                                Public                               -
+%-=====================================================================-
+
+%% @spec discover () -> ok
+%% @doc Initiate a discovery request.  Nodes will respond asynchronously
+%% and be added to the erlang node list subsequent to this call returning.
+%% @end
+
+discover() ->
+  nodefinder_server:discover().
+
+%-=====================================================================-
+%-                        application callbacks                        -
+%-=====================================================================-
+
+%% @hidden
+
+start() ->
+  crypto:start(),
+  application:start(nodefinder).
+
+%% @hidden
+
+start(_Type, _Args) ->
+  { ok, Addr } = application:get_env(nodefinder, addr),
+  { ok, Port } = application:get_env(nodefinder, port),
+  { ok, Ttl } = application:get_env(nodefinder, multicast_ttl),
+  nodefinder_sup:start_link (Addr, Port, Ttl).
+
+%% @hidden
+
+stop () ->
+  application:stop(nodefinder).
+
+%% @hidden
+
+stop (_State) ->
+  ok.
---
+++ src/nodefinder_sup.erl
@@ -0,0 +1,34 @@
+%% @hidden
+
+-module (nodefinder_sup).
+-behaviour (supervisor).
+
+-export ([ start_link/2, start_link/3, init/1 ]).
+
+%-=====================================================================-
+%-                                Public                               -
+%-=====================================================================-
+
+start_link (Addr, Port) ->
+  start_link (Addr, Port, 1).
+
+start_link (Addr, Port, Ttl) ->
+  supervisor:start_link (?MODULE, [ Addr, Port, Ttl ]).
+
+%-=====================================================================-
+%-                         Supervisor callbacks                        -
+%-=====================================================================-
+
+init ([ Addr, Port, Ttl ]) ->
+  { ok,
+    { { one_for_one, 3, 10 },
+      [ { nodefinder_server,
+          { nodefinder_server, start_link, [ Addr, Port, Ttl ] },
+          permanent,
+          1000,
+          worker,
+          [ nodefinder_server ]
+        }
+      ]
+    }
+  }.
